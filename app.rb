cbcb767bb84f0ffd1f98a56e26cc7571007e35e3
ik <ik@pixiv.com>
2015-08-11 14:17:19 +0900

    spec7: レシピの説明文を出力するように変更

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require 'yaml'
 
-def output(id, name)
-  puts "#{id}: #{name}"
+def output(id, name, description)
+  puts "#{id}: #{name} #{description}"
 end
 
 RECIPE_PATH = './recipes.yml'
@@ -13,10 +13,10 @@ recipe_id = ARGV[0]
 if recipe_id
   i = recipe_id.to_i - 1
   recipe = recipes[i]
-  output(recipe_id, recipe['name'])
+  output(recipe_id, recipe['name'], recipe['description'])
 else
   recipes.each_with_index do |recipe, i|
     id = i + 1
-    output(id, recipe['name'])
+    output(id, recipe['name'], recipe['description'])
   end
 end
