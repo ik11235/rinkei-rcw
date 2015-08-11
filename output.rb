7f72ab2190be9b2b608d3cea971166583b423833
Kei Hayashi <kei.h.hayashi@gmail.com>
2015-08-11 12:31:03 +0900

    Spec6: Enable id parameter to specify recipe

--- output.rb
+++ output.rb
@@ -4,7 +4,15 @@ RECIPE_PATH = './recipes.yml'
 
 recipes = YAML.load_file(RECIPE_PATH)
 
+recipe_id = ARGV[0]
+
 # 出力
-recipes.each_with_index do |recipe, i|
-  puts "#{i + 1}: #{recipe['name']}"
+if recipe_id
+  recipe = recipes[recipe_id.to_i - 1]
+  puts "#{recipe_id}: #{recipe['name']}"
+else
+  recipes.each_with_index do |recipe, i|
+    id = i + 1
+    puts "#{id}: #{recipe['name']}"
+  end
 end
