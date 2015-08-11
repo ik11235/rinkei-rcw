e06b690779d00be10829afa034629d869401fab3
Kei Hayashi <kei.h.hayashi@gmail.com>
2015-08-11 12:43:23 +0900

    Spec6: Add output method to unify format

--- app.rb
+++ app.rb
@@ -1,18 +1,22 @@
 require 'yaml'
 
+def output(id, name)
+  puts "#{id}: #{name}"
+end
+
 RECIPE_PATH = './recipes.yml'
 
 recipes = YAML.load_file(RECIPE_PATH)
 
 recipe_id = ARGV[0]
 
-# 出力
 if recipe_id
-  recipe = recipes[recipe_id.to_i - 1]
-  puts "#{recipe_id}: #{recipe['name']}"
+  i = recipe_id.to_i - 1
+  recipe = recipes[i]
+  output(recipe_id, recipe['name'])
 else
   recipes.each_with_index do |recipe, i|
     id = i + 1
-    puts "#{id}: #{recipe['name']}"
+    output(id, recipe['name'])
   end
 end
